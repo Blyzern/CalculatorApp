04c9f8ca307a518c1b49316f93935d1359c2d94d
Salvo <blyzer.game@gmail.com>
2022-05-10 03:18:54 +0200

    Feat/Calculato: Updating feature.

--- src/App.js
+++ src/App.js
@@ -19,26 +19,16 @@ import { Calculator } from './components/Calculator';
 //       platform: 'none'
 //   }
 //   ];
-const numbers = [1, 2 ,3, 4, 5, 6, 7, 8, 9, 0, '.'];
-const operators = ['+', '-', '*', '/','DEL'];
-const result = 0;
-
-
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
-        {
-          // users.map((items, index) => <UsersArray array={items}/>)
-          <div className='calculatorContainer'>
-            <div className='calculatorDisplay'>
-            <span>(0)</span>{result || '0'}
-              </div>
-            {operators.map((items, index) => <Calculator text={items}/>)}
-            {numbers.map((items, index) => <Calculator text={items} />)}
-          </div>
-        }
+          {/* users.map((items, index) => <UsersArray array={items}/>) */}
+          <Calculator />
+        
       </header>
     </div>
   );
--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Calculator.css';
 
-function addValue(number)
-{
-    if(number === '.' || (number >= 0 && number <= 9))
-    {
-        console.log(number);
+const CalculatorComponent = () =>{
+    const calculatorButtons = [1, 2 ,3, 4, 5, 6, 7, 8, 9, 0, '.','+', '-', '*', '/',];
+    const operators = ['+', '-', '*', '/', '.'];
+    const [calc, setCalc] = useState("");
+  
+    const handleClick = (e) => {
+        for (let i = 0; i < operators.length; i++) {
+            if (e.target.value === operators[i] && calc.includes(operators[i])) {
+                return;
+            }
+            if(e.target.value !== '-' && e.target.value === operators[i] && calc === "0"){
+                return;
+            }
+        }
+        if (e.target.value === '0' && calc === "0") {
+            clear();
+            return;
+        }
+        else{
+        setCalc(calc + e.target.value);
+        }
     }
-}
 
-export const Calculator = ({text}) => {
-    return <button className='calculatorButtons' onClick={() => addValue(text)}>{text}</button>
-};
+    const clear = () => {
+           setCalc("");
+    }
+    const equal = () => { 
+        setCalc(eval(calc).toString());
+    }
+
+    const buttonMaker = () => {
+        let buttons = [];
+        for (let i = 0; i < calculatorButtons.length; i++) {
+            buttons.push(<button key={i} value={calculatorButtons[i]} className='calculatorButtons' onClick={handleClick}>{calculatorButtons[i]}</button>)
+        }
+    return buttons;
+    }
+
+    return (
+        <div className='calculatorContainer'>
+            <div className='calculatorDisplay'>
+                {calc || '0'}
+            </div>
+                {buttonMaker()}
+                <button className='calculatorButtons' onClick={ clear }>DEL</button>
+                <button className='calculatorButtons' onClick={ equal }>=</button>
+        </div>
+       );
+    }
+
+
+export const Calculator = () => {
+    return CalculatorComponent();
+}

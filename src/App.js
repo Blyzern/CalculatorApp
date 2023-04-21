171ca78f35bcf25be7d81d0f15ee4d481fbd4970
Salvatore Mirabella <blyzer.game@gmail.com>
2023-04-21 19:06:04 +0200

    Fixing some css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,5 @@
 import './App.css';
 import { Calculator } from './components/Calculator';
-// import { UsersArray } from './components/UsersArray';
-
-// const users = [
-//   {
-//   name: 'Jonathan',
-//   age: '25',
-//   platform: 'Facebook'
-//   },
-//   {
-//       name: 'Salvatore',
-//       age: '21',
-//       platform: 'Twitter'
-//   },
-//   {
-//       name: 'Federico',
-//       age: '60',
-//       platform: 'none'
-//   }
-//   ];
 
 function App() {
   return (
